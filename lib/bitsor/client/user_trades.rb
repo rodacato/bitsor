cfa9237ad931f1feeaa1e44740acfc621cc12c03
Adrian Castillo <rodacato@gmail.com>
2017-11-26 09:39:18 -0600

    Fix another typo at user_trades

--- lib/bitsor/client/user_trades.rb
+++ lib/bitsor/client/user_trades.rb
@@ -1,7 +1,7 @@
 module Bitsor
   class Client
     module UserTrades
-      def user_trades(book:, makert: nil, sort: :desc, limit: 25)
+      def user_trades(book:, marker: nil, sort: :desc, limit: 25)
         get('/v3/user_trades/', book: book, marker: marker, sort: sort, limit: limit)
       end
     end
