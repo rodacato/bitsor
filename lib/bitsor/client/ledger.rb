0815dccba432dcb232807dbb10d205ab971edb21
Adrian Castillo <rodacato@gmail.com>
2017-11-26 09:07:54 -0600

    Fix typos on ledger endpoint

--- lib/bitsor/client/ledger.rb
+++ lib/bitsor/client/ledger.rb
@@ -2,23 +2,23 @@ module Bitsor
   class Client
     module Ledger
       def ledger(marker: nil, sort: :desc, limit: 25)
-        get('/v3/ledger', maker: maker, sort: sort, limit: limit)
+        get('/v3/ledger', marker: marker, sort: sort, limit: limit)
       end
 
       def ledger_trades(marker: nil, sort: :desc, limit: 25)
-        get('/v3/ledger/trades', maker: maker, sort: sort, limit: limit)
+        get('/v3/ledger/trades', marker: marker, sort: sort, limit: limit)
       end
 
       def ledger_fees(marker: nil, sort: :desc, limit: 25)
-        get('/v3/ledger/fees', maker: maker, sort: sort, limit: limit)
+        get('/v3/ledger/fees', marker: marker, sort: sort, limit: limit)
       end
 
       def ledger_fees(marker: nil, sort: :desc, limit: 25)
-        get('/v3/ledger/fundings', maker: maker, sort: sort, limit: limit)
+        get('/v3/ledger/fundings', marker: marker, sort: sort, limit: limit)
       end
 
       def ledger_fees(marker: nil, sort: :desc, limit: 25)
-        get('/v3/ledger/withdrawals', maker: maker, sort: sort, limit: limit)
+        get('/v3/ledger/withdrawals', marker: marker, sort: sort, limit: limit)
       end
     end
   end
